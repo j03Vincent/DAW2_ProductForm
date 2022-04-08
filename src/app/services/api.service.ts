aacdb01d8b41879ce9a49aa8a0b71f0907c25084
Vincent <joevincentg24@gmail.com>
2022-04-08 21:32:24 +0100

    fake json-server deployment

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,18 +8,18 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   postProduct(data: any) {
-    return this.http.post<any>('http://localhost:3000/productList/', data);
+    return this.http.post<any>('https://my-json-server.typicode.com/j03Vincent/DAW2_ProductList/productList', data);
   }
 
   getProduct() {
-    return this.http.get<any>('http://localhost:3000/productList/');
+    return this.http.get<any>('https://my-json-server.typicode.com/j03Vincent/DAW2_ProductList/productList');
   }
 
   putProduct(data: any, id: number) {
-    return this.http.put<any>('http://localhost:3000/productList/' + id, data);
+    return this.http.put<any>('https://my-json-server.typicode.com/j03Vincent/DAW2_ProductList/productList' + id, data);
   }
 
   deleteProduct(id: number) {
-    return this.http.delete<any>('http://localhost:3000/productList/' + id);
+    return this.http.delete<any>('https://my-json-server.typicode.com/j03Vincent/DAW2_ProductList/productList' + id);
   }
 }
